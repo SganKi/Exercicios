538d459fa3f24ff1160493eac6be016af1fb964e
Edson Pedro <edsonp18067@gmail.com>
2024-10-26 15:57:34 -0300

    Exercícios 01

---
+++ Aulas/exercicio_01.py
@@ -0,0 +1,32 @@
+'''
+1. Função: Saudação
+Escreva uma função chamada saudacao que receba um nome como argumento e imprima uma mensagem de boas-vindas.
+
+2. Função: Dobro de um Número
+Crie uma função que receba um número e retorne o dobro desse número.
+
+3. If/Else: Par ou Ímpar
+Escreva um programa que peça ao usuário um número e, em seguida, diga se o número é par ou ímpar.
+
+4. For: Contar até 10
+Crie um programa que use um loop for para imprimir os números de 1 a 10.
+    
+5. While: Contagem Regressiva
+Escreva um programa que faça uma contagem regressiva de 5 até 1, usando um loop while.
+
+6. Lista: Imprimir Elementos
+Crie uma lista com 5 frutas e use um loop for para imprimir cada uma delas.
+    
+7. Dicionário: Cores Favoritas
+Crie um dicionário com 3 pessoas e suas cores favoritas. Em seguida, peça o nome de uma pessoa e imprima a cor favorita dela.
+
+8. Função: Somar Dois Números
+Escreva uma função que receba dois números como argumentos e retorne a soma deles.
+
+9. If/Else: Verificar Maioridade
+Crie um programa que peça a idade do usuário e informe se ele é maior de idade (18 anos ou mais) ou menor de idade.
+
+10. Módulos: Mostrar Data Atual
+Use o módulo datetime para mostrar a data e a hora atual.
+
+'''
